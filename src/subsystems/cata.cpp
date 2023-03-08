896cda4c80da763aa0c47773377a4f220c6d50b5
BuildTools <unconfigured@null.spigotmc.org>
2023-03-07 18:40:58 -0800

    a

--- src/subsystems/cata.cpp
+++ src/subsystems/cata.cpp
@@ -3,34 +3,31 @@
 
 Motor cata(14, true, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
 ADIButton limSwitch('C', false);
+ bool cpsap =false;
+// void launch(void *){ //shoot function for auton
+// while (true)
+// {
+//   okapi::Rate rate;
+// while(limSwitch.isPressed()){ //lower cata until slip 
+//         cata.moveVoltage(12000);
+//        }
+//        cata.moveVoltage(0);//stop the cata 
 
-void launch(void *){ //shoot function for auton
-while (true)
-{
-  okapi::Rate rate;
-while(limSwitch.isPressed()){ //lower cata until slip 
+//         if(!limSwitch.isPressed()){
+//         cata.moveVoltage(12000); //move down unless button is pressed
+//        }
+//        if(limSwitch.isPressed()){
+//         cata.moveVoltage(0);
+//        }
+//        rate.delay(100_Hz); 
+// }
+// }
+void updateCata(){ 
+    if(controller.getDigital(ControllerDigital::R2)==1){
+      while(limSwitch.isPressed()){ //lower cata until slip 
         cata.moveVoltage(12000);
        }
        cata.moveVoltage(0);//stop the cata 
-
-        if(!limSwitch.isPressed()){
-        cata.moveVoltage(12000); //move down unless button is pressed
-       }
-       if(limSwitch.isPressed()){
-        cata.moveVoltage(0);
-       }
-       rate.delay(100_Hz); 
-}
-}
-void updateCata(){ 
-    if(controller.getDigital(ControllerDigital::R2)==1){
-       if(limSwitch.isPressed()){
-        cata.moveVoltage(12000);
-        pros::delay(50);
-       }
-    if(limSwitch.isPressed()){
-        cata.moveVoltage(0);
-       }
      
     }
       if((controller.getDigital(ControllerDigital::R2)==0)){
